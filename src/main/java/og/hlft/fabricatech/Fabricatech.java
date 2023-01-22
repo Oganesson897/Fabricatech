dab216515eb2fea988cc57cfb5a56af510375bf8
chemiofitor <783626185@qq.com>
2023-01-22 21:07:46 +0800

    fix fabric.mod.json

--- src/main/java/og/hlft/fabricatech/Fabricatech.java
+++ src/main/java/og/hlft/fabricatech/Fabricatech.java
@@ -1,10 +1,16 @@
 package og.hlft.fabricatech;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
-public class Fabricatech implements ModInitializer {
+public class Fabricatech implements ModInitializer, ClientModInitializer {
     @Override
     public void onInitialize() {
+
+    }
+
+    @Override
+    public void onInitializeClient() {
 
     }
 }
