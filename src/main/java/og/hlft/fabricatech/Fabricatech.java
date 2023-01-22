f791dd19d427ecd4bf45af1c4dd46e4c81f55992
Oganesson897 <darknight_123@qq.com>
2023-01-22 19:54:40 +0800

    Commit

---
+++ src/main/java/og/hlft/fabricatech/Fabricatech.java
@@ -0,0 +1,10 @@
+package og.hlft.fabricatech;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fabricatech implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
