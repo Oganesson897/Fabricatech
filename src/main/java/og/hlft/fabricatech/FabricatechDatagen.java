cb16dbc17feb48934a3fafbfe57b419b0173cd35
chemiofitor <783626185@qq.com>
2023-01-24 23:16:03 +0800

    LangGen

--- src/main/java/og/hlft/fabricatech/FabricatechDatagen.java
+++ src/main/java/og/hlft/fabricatech/FabricatechDatagen.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import og.hlft.fabricatech.datagen.RBlockLootProvider;
 import og.hlft.fabricatech.datagen.RModelProvider;
+import og.hlft.fabricatech.datagen.RI18NPProvider;
 
 import static og.hlft.fabricatech.Fabricatech.LOGGER;
 
@@ -12,6 +13,7 @@ public class FabricatechDatagen implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         generator.addProvider(RModelProvider::new);
         generator.addProvider(RBlockLootProvider::new);
+        generator.addProvider(RI18NPProvider.EnUs::new);
 
         LOGGER.info("Fabricatech dataGen done!");
     }
