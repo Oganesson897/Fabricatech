f257356ff75baae2a53da8c951ea69b5e7561779
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-01-30 18:33:10 +0800

    use space, not tab

--- src/main/java/og/hlft/fabricatech/datagen/RBlockLootProvider.java
+++ src/main/java/og/hlft/fabricatech/datagen/RBlockLootProvider.java
@@ -36,7 +36,8 @@ public class RBlockLootProvider extends SimpleFabricLootTableProvider {
         for (RMaterialPart part : material.getBlockParts()) {
             if (part == RMaterialPart.ORE || part == RMaterialPart.DEEPSLATE_ORE)
                 continue;
-            consumer.accept(asId(material.makeRID(part)), BlockLootTableGenerator.drops(material.getPartBlock(part).asItem(), ConstantLootNumberProvider.create(1)));
+            consumer.accept(asId(material.makeRID(part)), BlockLootTableGenerator
+                    .drops(material.getPartBlock(part).asItem(), ConstantLootNumberProvider.create(1)));
         }
     }
 }
